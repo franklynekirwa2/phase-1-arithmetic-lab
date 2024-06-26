18d06f1d326dfc63c786fe92a29adb8b0e9f2442
franklynekirwa2 <sangkirwa2@gmail.com>
2024-06-26 19:48:36 +0300

    arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+let num1=31;
+let num2=2;
+let multiply=num1*num2;
+console.log(multiply);
 
-// Write your code here
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+ console.log(random);
+
+  let num3=24;
+  let num4=20;
+  let mod=num3 % num4;
+  console.log(mod);
+
+
+  let max = Math.max(3,9, 13, 17,20);
+
+console.log(max);
+
